d2cd855ca83de048aa1b497dcf7890b8b30a9202
Andreas Holzapfel <holzapfel.a1994@gmail.com>
2023-03-31 09:41:49 +0200

    added comments

--- lib/pages/routetrees/authtree.dart
+++ lib/pages/routetrees/authtree.dart
@@ -4,6 +4,11 @@ import 'package:nexus_wallet/pages/auth/loginscreen.dart';
 import 'package:nexus_wallet/pages/auth/registerscreen.dart';
 import 'package:nexus_wallet/pages/auth/resetpasswordscreen.dart';
 
+/*
+ This Flutter widget displays either a login screen, a registration screen,
+ or a reset password screen based on the values of two boolean variables: showSignIn and resetpassword.
+ It contains two callback functions to toggle the values of these variables.
+ */
 class AuthTree extends StatefulWidget {
   const AuthTree({Key? key}) : super(key: key);
 
@@ -12,9 +17,11 @@ class AuthTree extends StatefulWidget {
 }
 
 class _AuthTreeState extends State<AuthTree> {
+  // boolean variables to track which screen to show
   bool showSignIn = true;
   bool resetpassword = false;
 
+  // callback function to switch between sign-in and registration screens
   void toggleView() {
     setState(() {
       if (showSignIn == true)
@@ -25,6 +32,7 @@ class _AuthTreeState extends State<AuthTree> {
     });
   }
 
+  // callback function to switch between sign-in and reset password screens
   void toggleresetpassword() {
     setState(() {
       if (resetpassword == true)
@@ -37,18 +45,21 @@ class _AuthTreeState extends State<AuthTree> {
 
   @override
   Widget build(BuildContext context) {
+    // show the reset password screen if the resetpassword variable is true
     if (resetpassword) {
       return ResetPasswordScreen(
         toggleView: toggleView,
         toggleResetPassword: toggleresetpassword,
       );
     } else {
+      // show the sign-in screen if the showSignIn variable is true
       if (showSignIn) {
         return LoginScreen(
           toggleView: toggleView,
           toggleResetPassword: toggleresetpassword,
         );
       } else {
+        // show the registration screen if both variables are false
         return RegisterScreen(
           toggleView: toggleView,
         );
