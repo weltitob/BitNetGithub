86d9a35e763fa0256d78252c841dd28ece6cce21
Andreas Holzapfel <holzapfel.a1994@gmail.com>
2023-03-31 10:48:18 +0200

    added comments

--- lib/backbone/security/security.dart
+++ lib/backbone/security/security.dart
@@ -1,16 +1,17 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
-// checks the authentificationstatus with SharedPreferences
+// Function to retrieve the security check status from shared preferences
 Future<bool> SharedPrefSecurityCheck() async {
-
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  bool isLoggedIn = prefs.getBool('isSecurityChecked') ?? false;
+  SharedPreferences prefs = await SharedPreferences.getInstance(); // Get the SharedPreferences instance
+  bool isLoggedIn = prefs.getBool('isSecurityChecked') ?? false; // Read the boolean value of the 'isSecurityChecked' key. If it doesn't exist, return false.
   return isLoggedIn;
 }
 
+// Function to check the security status and enforce security measures if necessary
 Future<void> checkSecurityStatus() async {
-  bool isSecurityChecked = await SharedPrefSecurityCheck();
+  bool isSecurityChecked = await SharedPrefSecurityCheck(); // Check if the security has been checked before
   if (isSecurityChecked) {
+    // If the security has been checked before, do nothing
   } else {
   }
 }
