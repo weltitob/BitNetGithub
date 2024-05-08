da8d584404c7233066b554e3e540e7ed68119792
abberatfortex <abberatfortex>
2024-05-08 19:25:35 +0500

    Add the fees to the lightning transaction screen

--- lib/components/resultlist/transactions.dart
+++ lib/components/resultlist/transactions.dart
@@ -24,7 +24,7 @@ import 'package:permission_handler/permission_handler.dart';
 
 class Transactions extends StatefulWidget {
   bool fullList;
-   Transactions({Key? key, this.fullList = false}) : super(key: key);
+  Transactions({Key? key, this.fullList = false}) : super(key: key);
 
   @override
   State<Transactions> createState() => _TransactionsState();
@@ -32,7 +32,6 @@ class Transactions extends StatefulWidget {
 
 class _TransactionsState extends State<Transactions>
     with SingleTickerProviderStateMixin {
-
   bool transactionsLoaded = false;
   List<LightningPayment> lightningPayments = [];
   List<ReceivedInvoice> lightningInvoices = [];
@@ -42,7 +41,8 @@ class _TransactionsState extends State<Transactions>
   void getOnchainTransactions() async {
     Logs().w("Getting onchain transactions");
     RestResponse restBitcoinTransactions = await getTransactions();
-    BitcoinTransactionsList bitcoinTransactions = BitcoinTransactionsList.fromJson(restBitcoinTransactions.data);
+    BitcoinTransactionsList bitcoinTransactions =
+        BitcoinTransactionsList.fromJson(restBitcoinTransactions.data);
     onchainTransactions = bitcoinTransactions.transactions;
     setState(() {});
   }
@@ -51,7 +51,8 @@ class _TransactionsState extends State<Transactions>
   void getLightningPayments() async {
     Logs().w("Getting lightning payments");
     RestResponse restLightningPayments = await listPayments();
-    LightningPaymentsList lightningPayments = LightningPaymentsList.fromJson(restLightningPayments.data);
+    LightningPaymentsList lightningPayments =
+        LightningPaymentsList.fromJson(restLightningPayments.data);
     this.lightningPayments = lightningPayments.payments;
     setState(() {});
   }
@@ -60,14 +61,15 @@ class _TransactionsState extends State<Transactions>
   void getLightningInvoices() async {
     Logs().w("Getting lightning invoices");
     RestResponse restLightningInvoices = await listInvoices();
-    ReceivedInvoicesList lightningInvoices = ReceivedInvoicesList.fromJson(restLightningInvoices.data);
-    List<ReceivedInvoice> settledInvoices = lightningInvoices.invoices.where((invoice) => invoice.settled).toList();
+    ReceivedInvoicesList lightningInvoices =
+        ReceivedInvoicesList.fromJson(restLightningInvoices.data);
+    List<ReceivedInvoice> settledInvoices =
+        lightningInvoices.invoices.where((invoice) => invoice.settled).toList();
     this.lightningInvoices = settledInvoices;
     setState(() {});
   }
 
   // Subscriptions
-
 
   @override
   void initState() {
@@ -87,7 +89,6 @@ class _TransactionsState extends State<Transactions>
 
   @override
   void dispose() {
-
     super.dispose();
   }
 
@@ -95,20 +96,21 @@ class _TransactionsState extends State<Transactions>
   Widget build(BuildContext context) {
     var combinedTransactions = [
       ...lightningInvoices.map((transaction) => TransactionItem(
-        context: context,
-        data: TransactionItemData(
+          context: context,
+          data: TransactionItemData(
             timestamp: transaction.settleDate,
             type: TransactionType.lightning,
             direction: TransactionDirection.received,
             receiver: transaction.paymentRequest.toString(),
             txHash: transaction.value.toString(),
             amount: "+" + transaction.amtPaid.toString(),
+            fee: 0,
             status: transaction.settled
                 ? TransactionStatus.confirmed
                 : TransactionStatus.failed,
 
             // other properties
-    ))),
+          ))),
       ...lightningPayments.map((transaction) => TransactionItem(
           context: context,
           data: TransactionItemData(
@@ -118,77 +120,107 @@ class _TransactionsState extends State<Transactions>
             receiver: transaction.paymentHash.toString(),
             txHash: transaction.paymentHash.toString(),
             amount: "-" + transaction.valueSat.toString(),
+            fee: transaction.fee,
             status: transaction.status == "SUCCEEDED"
-                ? TransactionStatus.confirmed : transaction.status == "FAILED"
-                ? TransactionStatus.failed
-                : TransactionStatus.pending,
+                ? TransactionStatus.confirmed
+                : transaction.status == "FAILED"
+                    ? TransactionStatus.failed
+                    : TransactionStatus.pending,
           ))),
       ...onchainTransactions.map((transaction) => TransactionItem(
-    context: context,
+          context: context,
           data: TransactionItemData(
-          timestamp: transaction.timeStamp,
-          status: transaction.numConfirmations > 0 ? TransactionStatus.confirmed : TransactionStatus.pending,
-          type: TransactionType.onChain,
-          direction: transaction.amount.contains("-")
-              ? TransactionDirection.sent
-              : TransactionDirection.received,
-          receiver: transaction.amount.contains("-")
-              ? transaction.destAddresses.last.toString()
-              : transaction.destAddresses.first.toString(),
-          txHash: transaction.txHash.toString(),
-          amount: transaction.amount.contains("-")
-              ? transaction.amount.toString()
-              : "+" + transaction.amount.toString(),
-
+            timestamp: transaction.timeStamp,
+            status: transaction.numConfirmations > 0
+                ? TransactionStatus.confirmed
+                : TransactionStatus.pending,
+            type: TransactionType.onChain,
+            direction: transaction.amount.contains("-")
+                ? TransactionDirection.sent
+                : TransactionDirection.received,
+            receiver: transaction.amount.contains("-")
+                ? transaction.destAddresses.last.toString()
+                : transaction.destAddresses.first.toString(),
+            txHash: transaction.txHash.toString(),
+            fee: 0,
+            amount: transaction.amount.contains("-")
+                ? transaction.amount.toString()
+                : "+" + transaction.amount.toString(),
           ))),
     ].toList();
 
-    combinedTransactions.sort((a, b) => a.data.timestamp.compareTo(b.data.timestamp));
+    combinedTransactions
+        .sort((a, b) => a.data.timestamp.compareTo(b.data.timestamp));
     combinedTransactions = combinedTransactions.reversed.toList();
 
-    return transactionsLoaded  ? widget.fullList ? bitnetScaffold(
-      context: context,
-      extendBodyBehindAppBar: true,
-      appBar: bitnetAppBar(context: context, text: 'Activity', onTap: (){Navigator.pop(context);},),
-      body: Padding(
-        padding: const EdgeInsets.only(top: 40.0),
-        child: Column(
-          children: [
-            //SizedBox(height: 80,),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 10.0),
-              child: SearchFieldWidget(
-                // controller: searchCtrl,
-                 hintText: 'Search',
-                handleSearch:(v){
-                  setState(() {
-                    searchCtrl.text = v;
-                  });
-                },
-                suffixIcon:  IconButton(
-                  // size: AppTheme.cardPadding * 1.25,
-                  // buttonType: ButtonType.transparent,
-                  icon: Icon(FontAwesomeIcons.filter),
-                  onPressed: () {
-                    BitNetBottomSheet(
-                        context: context,
-                        child: WalletFilterScreen());
-                  }),
-                isSearchEnabled: true,),
-            ),
-            Expanded(child: ListView.builder(
-                padding: EdgeInsets.zero,
-                physics: NeverScrollableScrollPhysics(),
-                itemCount: combinedTransactions.length,
-                itemBuilder:(context, index)=> combinedTransactions[index].data.receiver.contains(searchCtrl.text.toLowerCase()) ? combinedTransactions[index] : SizedBox())),
-          ],
-        ),
-      )
-    ) : Container(
-        height: AppTheme.cardPadding * 18,
-        child: ListView.builder(
-            physics: NeverScrollableScrollPhysics(),
-            itemCount: combinedTransactions.length > 5 ? 5 : combinedTransactions.length,
-            itemBuilder:(context, index)=> combinedTransactions[index])) : Container(height: AppTheme.cardPadding * 18, child: dotProgress(context),);
+    return transactionsLoaded
+        ? widget.fullList
+            ? bitnetScaffold(
+                context: context,
+                extendBodyBehindAppBar: true,
+                appBar: bitnetAppBar(
+                  context: context,
+                  text: 'Activity',
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                body: Padding(
+                  padding: const EdgeInsets.only(top: 40.0),
+                  child: Column(
+                    children: [
+                      //SizedBox(height: 80,),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 20.0, horizontal: 10.0),
+                        child: SearchFieldWidget(
+                          // controller: searchCtrl,
+                          hintText: 'Search',
+                          handleSearch: (v) {
+                            setState(() {
+                              searchCtrl.text = v;
+                            });
+                          },
+                          suffixIcon: IconButton(
+                              // size: AppTheme.cardPadding * 1.25,
+                              // buttonType: ButtonType.transparent,
+                              icon: Icon(FontAwesomeIcons.filter),
+                              onPressed: () {
+                                BitNetBottomSheet(
+                                    context: context,
+                                    child: WalletFilterScreen());
+                              }),
+                          isSearchEnabled: true,
+                        ),
+                      ),
+                      Expanded(
+                          child: ListView.builder(
+                              padding: EdgeInsets.zero,
+                              physics: NeverScrollableScrollPhysics(),
+                              itemCount: combinedTransactions.length,
+                              itemBuilder: (context, index) =>
+                                  combinedTransactions[index]
+                                          .data
+                                          .receiver
+                                          .contains(
+                                              searchCtrl.text.toLowerCase())
+                                      ? combinedTransactions[index]
+                                      : SizedBox())),
+                    ],
+                  ),
+                ))
+            : Container(
+                height: AppTheme.cardPadding * 18,
+                child: ListView.builder(
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: combinedTransactions.length > 5
+                        ? 5
+                        : combinedTransactions.length,
+                    itemBuilder: (context, index) =>
+                        combinedTransactions[index]))
+        : Container(
+            height: AppTheme.cardPadding * 18,
+            child: dotProgress(context),
+          );
   }
 }
