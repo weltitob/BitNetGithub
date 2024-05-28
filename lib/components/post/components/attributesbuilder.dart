c3e7c933dd44450703ab066cb07fa39de16bbd04
Tobias Welti <95380067+weltitob@users.noreply.github.com>
2024-05-28 17:27:29 +0200

    V 3.5.9
    - attributes updates

--- lib/components/post/components/attributesbuilder.dart
+++ lib/components/post/components/attributesbuilder.dart
@@ -1,14 +1,13 @@
 import 'dart:convert';
-import 'dart:typed_data';
-
-import 'package:bitnet/backbone/helper/theme/theme.dart';
-import 'package:bitnet/components/post/components/postfile_model.dart';
 import 'package:flutter/material.dart';
+import 'package:bitnet/components/marketplace_widgets/CommonHeading.dart';
+import 'package:bitnet/components/marketplace_widgets/PropertieList.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-
-//USED FOR UPLOAD SCREEN (USER PICKS FILE LOCALLY)
+// USED FOR UPLOAD SCREEN (USER PICKS FILE LOCALLY)
 class AttributesBuilder extends StatelessWidget {
   final String attributes;
+
   const AttributesBuilder({
     Key? key,
     required this.attributes,
@@ -16,9 +15,58 @@ class AttributesBuilder extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //cinvert the data to a map and then create a widget for each attribute
-    print("Attributes should be displayed here: $attributes");
-    return Text("Attributes should be displayed here: $attributes");
+    List<Map<String, String>> propertyList = [];
+
+    try {
+      final List<dynamic> jsonData = json.decode(attributes);
+      final propertyList = jsonData.map((item) => {
+        'trait_type': item['trait_type'],
+        'value': item['value']
+      }).toList();
+    } catch (e) {
+      return Container(
+        padding: EdgeInsets.symmetric(horizontal: 20.w),
+        child: CommonHeading(
+          headingText: 'Properties',
+          hasButton: false,
+          collapseBtn: true,
+          child: Container(
+            margin: EdgeInsets.only(bottom: 30.w),
+            child: Text('Error parsing attributes: $e $attributes'),
+          ),
+        ),
+      );
+    }
+
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.w),
+      child: CommonHeading(
+        headingText: 'Properties',
+        hasButton: false,
+        collapseBtn: true,
+        child: Container(
+          margin: EdgeInsets.only(bottom: 30.w),
+          child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              childAspectRatio: 1.7, // 9.1 / 4.5
+              crossAxisSpacing: 15,
+              mainAxisSpacing: 15,
+              crossAxisCount: 2,
+            ),
+            physics: const NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            padding: EdgeInsets.zero,
+            itemCount: propertyList.length,
+            itemBuilder: (BuildContext context, int index) {
+              return PropertieList(
+                heading: propertyList[index]['trait_type'] ?? '',
+                subHeading: propertyList[index]['value'] ?? '',
+                peragraph: '',
+              );
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
-
